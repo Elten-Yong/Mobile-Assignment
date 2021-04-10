53a656356f769bdbdf917d6c5ec078767b5ea00d
teejoohong0823 <teejh-wp18@student.tarc.edu.my>
2021-04-10 23:56:34 +0800

    jh

---
+++ app/src/main/java/com/example/androidassignment/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidassignment.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
