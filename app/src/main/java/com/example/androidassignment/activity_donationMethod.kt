fed78a7e61dd2ee2e22e41212caf7df13d8e4246
samson <samso@DESKTOP-COMLFS9>
2021-04-14 22:43:08 +0800

    commit by jx

---
+++ app/src/main/java/com/example/androidassignment/activity_donationMethod.kt
@@ -0,0 +1,11 @@
+package com.example.androidassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_donationMethod : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation_method)
+    }
+}
