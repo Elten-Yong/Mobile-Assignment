944a3c1422249882b11ca9281ff17d05ab8a8b73
samson <samso@DESKTOP-COMLFS9>
2021-04-16 23:09:57 +0800

    commit by jx

--- app/src/main/java/com/example/androidassignment/cardInformationActivity.kt
+++ app/src/main/java/com/example/androidassignment/cardInformationActivity.kt
@@ -3,7 +3,7 @@ package com.example.androidassignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_card_information : AppCompatActivity() {
+class cardInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_information)
